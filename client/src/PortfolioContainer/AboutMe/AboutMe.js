8f23881ed130dd10a9c300e68b7efc8513ec60cb
ChiragTanksali <chirag.tanksali@gmail.com>
2022-08-05 17:41:55 +0530

    first commit

---
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
+import ScrollService from '../../utilities/ScrollService';
+import Animations from '../../utilities/Animations';
+import "./AboutMe.css"
+
+export default function AboutMe(props) {
+    let fadeInScreenHandler = (screen) => {
+        if (screen.fadeInScreen !== props.id)
+            return
+        Animations.animations.fadeInScreen(props.id)
+    }
+    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+
+    const SCREEN_CONSTANTS = {
+        description: " I'm Chirag Tanksali  Currently studying Bachelor of Engineering at KLS Gogte Institute of Technology, Belagavi. I'm a Creative Full Stack Web Developer as well as an App developer with Background knowledge of MERN Stacks and React Native .Also, I'm passionate about developing  User-friendly Websites that improves the lives of those around me and I also like solving Competitive programming questions",
+        highlights: {
+            bullets: [
+                "Front and Back end Developer",
+                "MERN Stack Developer",
+                "App Developer",
+                "React and React Native Developer",
+                // "Managing Database",
+            ],
+            heading: "Here are a Few Highlights:"
+        }
+    }
+
+    const renderHighlight = () => {
+        return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+            <div className="highlight" key={i}>
+                <div className="highlight-blob"></div>
+                <span>{value}</span>
+            </div>
+        ));
+    };
+
+
+
+    return (
+        <div
+            className="about-me-container screen-container fade-in " data-aos-duration="2000"
+            id={props.id || ""}
+        >
+            <div className="about-me-parent">
+                <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
+                <div className="about-me-card">
+                    <div className="about-me-profile"></div>
+                    <div className="about-me-details">
+                        <span className="about-me-description">
+                            {SCREEN_CONSTANTS.description}
+                        </span>
+                        <div className="about-me-highlights">
+                            <div className="highlight-heading">
+                                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+                            </div>
+                            {renderHighlight()}
+                        </div>
+                        <div className="about-me-options">
+                            <a href="https://drive.google.com/file/d/1sRNstWkHM8LHL_LgEtUpCPNXyRVS8j0b/view?usp=sharing" class="btn" target="_blank"> <button className='btn highlighted-btn'>Get Resume</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ client/src/PortfolioContainer/Projects/Projects.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import OwlCarousel from 'react-owl-carousel'
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
+import ScrollService from '../../utilities/ScrollService';
+import Animations from '../../utilities/Animations';
+import './Projects.css';
+
+
+export default function Projects(props) {
+
+    let fadeInScreenHandler = (screen) => {
+        if (screen.fadeInScreen !== props.id)
+            return
+        Animations.animations.fadeInScreen(props.id)
+    };
+    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+    const options = {
+        loop: true,
+        margin: 0,
+        nav: true,
+        animateIn: "bounceInRight",
+        animateOut: "bounceOutRight",
+        dots: true,
+        autoplay: true,
+        smartSpeed: 10000,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 1,
+            },
+            1000: {
+                items: 3,
+            },
+        },
+    };
+
+    return (
+        <div>
+            <ScreenHeading
+                title={'Projects'}
+                subHeading={'My Extra Curricular Project Details'}
+            />
+            <section className='projects-section fade-in' id={props.id || ''}>
+                <div className="container">
+                    <div className="role">
+                        <OwlCarousel className='owl-carousel' id='projects-carousel' {...options}>
+                            <div className='col-lg-12'>
+                                <div className='project-item'>
+                                    <div className='project-comment'>
+                                        <h3>Discuss-mates : Video and Voice call Web application</h3>
+                                        <p>
+                                            Built using WebRTC,Socket.IO and Javascript.Right from the landing page where users will able to host a new meeting or will be able to join in an existing meeting by using meeting code to advanced features like creating audio and video streaming,Live Chat,Sharing screen,Recording system etc!
+                                        </p>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            <div className='col-lg-12'>
+                                <div className='project-item'>
+                                    <div className='project-comment'>
+                                        <h3>Personal Portfolio Website</h3>
+                                        <p>
+                                            Developed a Responsive portfolio website with email functionality using React.js,Bootstrap,rxjs and NodeJS. Implemented  Smooth Scrolling and Fade In Animation and react type effect changing different text based on timing and call to action button using Rxjs.Deployed website on Heroku Server.
+                                        </p>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            <div className='col-lg-12'>
+                                <div className='project-item'>
+                                    <div className='project-comment'>
+                                        <h3>Expense Tracker Mobile App</h3>
+                                        <p>
+                                        A cross-platformed (iOS and Android) Mobile Application that helps users to track and manage their total expense.Developed using React Native .Built with the purpose to learn the use of React Navigation and to implement CRUD operations using firebase (Real time Data Storage in firestore database).
+                                        </p>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            <div className='col-lg-12'>
+                                <div className='project-item'>
+                                    <div className='project-comment'>
+                                        <h3>CV / Resume Analysis using Python</h3>
+                                        <p>
+                                        Developed a  Python application which finds relevant fields of interest in which a candidate has got expertise and classify the relevant skills accordingly in a Pie chart . Thus , Resume screening saves recruiters time during the hiring process and leads to faster onboarding 
+of new employees.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-lg-12'>
+                                <div className='project-item'>
+                                    <div className='project-comment'>
+                                        <h3>Easy Shop : Ecommerce Web Application</h3>
+                                        <p>
+                                            Used Redux for changing the states, Managing and Storing Database in the cloud.
+                                            Implemented functionalities like custom Admin Panel area to manage products,user and orders to update info and other
+                                            functionalities like JWT authentication,Product Search etc..
+                                        </p>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </OwlCarousel>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    )
+}
